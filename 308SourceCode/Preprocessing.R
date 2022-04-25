a66f93c7d5237ba2280cb16f68fef29951923bf1
Jiacheng Zhu <jiachengzhu0402@gmail.com>
2022-04-24 23:16:43 -0400

    Loading all the data - make data ready for preprocessing

--- 308SourceCode/Preprocessing.R
+++ 308SourceCode/Preprocessing.R
@@ -1,11 +1,15 @@
+# library
 library(data.table)
 library(tidyverse)
 
-df <- fread('/Users/jiachengzhu/Desktop/project/gun-violence-data_01-2013_03-2018.csv')
-df2 <- fread('/Users/jiachengzhu/Desktop/export-d08ddc4a-511b-4e6b-8398-65d3e063a331.csv')
-df3 <- fread('/Users/jiachengzhu/Desktop/SoDA-308-Project/308SourceCode/RawData/gun-violence-data_01-2013_03-2018.csv')
-
-df3 <- df3 %>%
-  select(!c(incident_url,source_url,sources))
-
-fwrite(df3,'./RawData/historical-violence-data.csv')
+# load all the datasets
+Gun2019 <- fread('./308SourceCode/RawData/2019Gun.csv')
+AllYearGun <- fread('./308SourceCode/RawData/allYearGun.csv')
+education <- fread('./308SourceCode/RawData/Education.csv')
+population <- fread('./308SourceCode/RawData/PopulationEstimates.csv')
+unemployment <- fread('./308SourceCode/RawData/Unemployment.csv')
+hisViolence <- fread('./308SourceCode/RawData/historical-violence-data.csv')
+income <- fread('./308SourceCode/RawData/Income.csv')
+job <- fread('./308SourceCode/RawData/Jobs.csv')
+veterans <- fread('./308SourceCode/RawData/Veterans.csv')
+people <- fread('./308SourceCode/RawData/People.csv')
